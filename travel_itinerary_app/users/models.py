7262c56389853932ba068f3676babf67c81d053b
Jahnavi Gogia <jahnavigogia02@gmail.com>
2025-11-16 03:13:34 +0530

    add .gitignore

--- travel_itinerary_app/users/models.py
+++ travel_itinerary_app/users/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 import uuid
 
-# Create your models here.
 class CustomUser(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     phone_no = models.CharField(max_length=20, blank=True, null=True)
